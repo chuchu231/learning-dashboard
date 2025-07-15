3f3cad64e6a0a985eb38dfd24b6397f0d5824df6
nltleechu <96467968+chuchu231@users.noreply.github.com>
2025-07-15 14:13:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -529,9 +529,13 @@ elif selected == "Learning Performance":
     if not df_ranks.empty:
         st.write(df_ranks)
         class_id = df_scores["Class SK"].iloc[0]
+        score_ranges = [f"{i}-{i+1}" for i in range(10)]  # Tạo 10 nhãn từ 0-1 đến 9-10
+
+        # Truy xuất dữ liệu đúng thứ tự đã sắp
+        student_counts = df_ranks.loc[0, rank_cols_sorted].values
         df_plot = pd.DataFrame({
-            "Score Range": [f"{i}-{i+1}" for i in range(10)],
-            "Number of students": df_ranks.iloc[0, 2:].values
+            "Score Range": score_ranges,
+            "Number of students":  student_counts
         })
 
         fig_ranks = px.bar(
