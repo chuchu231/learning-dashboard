6426c14f0496b1c549d57b0e1b5e3f0d0ef3492d
nltleechu <96467968+chuchu231@users.noreply.github.com>
2025-07-22 09:15:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 # ---------------------- Gọi API ----------------------
-BASE_URL = "https://feb4dd14683d.ngrok-free.app"
+BASE_URL = "https://5c1a53863064.ngrok-free.app"
 
 def load_class_list():
     try:
