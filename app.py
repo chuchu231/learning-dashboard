7450d47c12a56ca5abd0c831e438768af240d83d
nltleechu <96467968+chuchu231@users.noreply.github.com>
2025-07-23 12:39:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1106,7 +1106,6 @@ elif selected == "Correlation Analysis":
 
             with col3:
                 df = df_quiz_score
-                st.write(df)
                 if not df.empty:
                     render_scatter_chart(
                         df,
