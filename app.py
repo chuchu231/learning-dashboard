244e5b1d627a3540b53347faa59c095afb7a8d1c
nltleechu <96467968+chuchu231@users.noreply.github.com>
2025-07-27 23:51:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -334,7 +334,7 @@ elif selected == "Learning Performance":
             margin-bottom: 0.5rem;
         }
         .metric-value {
-            font-size: 1.75rem; 
+            font-size: 2rem; 
             font-weight: 700;
         }
         .metric-value.avg-value {
