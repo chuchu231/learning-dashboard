5779e4e7a218977be143fb2a48995f37bcb18bf9
nltleechu <96467968+chuchu231@users.noreply.github.com>
2025-07-27 23:21:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -666,7 +666,7 @@ elif selected == "Learning Behavior":
         .stMetric {
             background-color: #ffffff;
             border-radius: 16px;
-            padding: 1.5rem;
+            padding: 1rem;
             box-shadow: 0 4px 12px rgba(0,0,0,0.05);
             text-align: left;
             transition: transform 0.2s;
